e394be7d2466b9f24f9a70e67dbd2ab8961bfcd5
Joe Gatt <joe@joegatt.net>
2012-05-05 13:25:25 +0100

    New Rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nembrot::Application.config.secret_token = '6c62d4f9d487f4b79c16b9e08ee552a1e7592bc59e2162f5ec86c64a02614f7bc5b04ba6693e9384fba4d39011df830dc84604fc58a9dd4ab21ac38fba07c4c1'

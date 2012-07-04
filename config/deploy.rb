86e505f46c04fc0b7d786cb9d18571ecd1f1de9e
Joe Gatt <joe@joegatt.net>
2012-07-04 02:31:02 +0100

    Precompiles assets.

--- config/deploy.rb
+++ config/deploy.rb
@@ -28,9 +28,9 @@ set(:previous_revision) { capture("cd #{current_path}; git rev-parse --short HEA
 default_environment["RAILS_ENV"] = 'production'
 
 # Use our ruby-1.9.2-p290@my_site gemset
-default_environment["PATH"]         = "/usr/local/rvm/gems/ruby-1.9.2-p290@joegattnet_v3/bin:/usr/local/rvm/gems/ruby-1.9.2-p290@global/bin:/usr/local/rvm/rubies/ruby-1.9.2-p290/bin:/usr/local/rvm/bin:$PATH"
+default_environment["PATH"]         = "/usr/local/rvm/gems/ruby-1.9.2-p290@joegattnet_v3/bin:/usr/local/rvm/gems/ruby-1.9.2-p290@joegattnet_v3/bin:/usr/local/rvm/rubies/ruby-1.9.2-p290/bin:/usr/local/rvm/bin:$PATH"
 default_environment["GEM_HOME"]     = "/usr/local/rvm/gems/ruby-1.9.2-p290@joegattnet_v3"
-default_environment["GEM_PATH"]     = "/usr/local/rvm/gems/ruby-1.9.2-p290@joegattnet_v3:/usr/local/rvm/gems/ruby-1.9.2-p290@global"
+default_environment["GEM_PATH"]     = "/usr/local/rvm/gems/ruby-1.9.2-p290@joegattnet_v3:/usr/local/rvm/gems/ruby-1.9.2-p290@joegattnet_v3"
 default_environment["RUBY_VERSION"] = "ruby-1.9.2-p290"
 
 default_run_options[:shell] = 'bash'

2eac682efb1e4dd8119897678b5bcc4c16584a18
Joe Gatt <joe@joegatt.net>
2012-07-28 02:23:57 +0100

    Adds 'version required' to model.

--- app/models/note_version.rb
+++ app/models/note_version.rb
@@ -1,10 +1,11 @@
 class NoteVersion < ActiveRecord::Base
   attr_accessible :body, :title, :note_id, :version
   belongs_to :note
-
+  
 # validates :note_id, :presence => true
 # we may not know it at this stage
   validates :title, :presence => true
   validates :body, :presence => true
+  validates :version, :presence => true
 
 end

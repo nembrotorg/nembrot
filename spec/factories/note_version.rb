13706ba8eddc1076ac5dc9599f3c4d5cc7381455
Joe Gatt <joe@joegatt.net>
2012-07-28 02:17:53 +0100

    Adds tests for NoteVersion.

---
+++ spec/factories/note_version.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+    factory :note_version do
+    title "This the Title of Our Version"
+    body "This is then the body, innit."
+    version 1
+    note_id 21
+  end
+end
---
+++ spec/models/note_version_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+describe NoteVersion do
+	it "has a valid factory" do
+		FactoryGirl.create( :note_version ).should be_valid
+	end
+	# it "is invalid without an note_id" do
+	# 	FactoryGirl.build( :note_version, note_id: nil).should_not be_valid
+	# end
+	it "is invalid without a title" do
+		FactoryGirl.build( :note_version, title: nil).should_not be_valid
+	end
+	it "is invalid without a body" do
+		FactoryGirl.build( :note_version, body: nil).should_not be_valid
+	end
+end

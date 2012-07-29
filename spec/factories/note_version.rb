5bc260d012dc750d1b67f8284c066694dd8fd47a
Joe Gatt <joe@joegatt.net>
2012-07-29 21:27:52 +0100

    Using Faker for content in factories.

--- spec/factories/note_version.rb
+++ spec/factories/note_version.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
-    factory :note_version do
-    title "This the Title of Our Version"
-    body "This is then the body, innit."
-    version 1
-    note_id 21
-  end
+	factory :note_version do
+    	title { Faker::Lorem.sentences(1) }
+    	body { Faker::Lorem.paragraphs(5) }
+    	version 1
+    	note_id 1
+	end
 end

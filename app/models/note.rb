6fea54e544ce6e7d3e9323218dd1c49345ab9de3
Joe Gatt <joe@joegatt.net>
2012-07-30 00:26:14 +0100

    Adds nesting of Notes and NoteVersions (on create).

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,5 +1,7 @@
 class Note < ActiveRecord::Base
   attr_accessible :external_note_id
 
+  has_many :note_versions, :dependent => :destroy
+
   validates	:external_note_id, :presence => true
 end
--- app/models/note_version.rb
+++ app/models/note_version.rb
@@ -1,11 +1,24 @@
 class NoteVersion < ActiveRecord::Base
-  attr_accessible :body, :title, :note_id, :version
+  attr_accessible :body, :title, :note_id, :version, :external_note_id
   belongs_to :note
   
+  attr_writer :external_note_id
+
 # validates :note_id, :presence => true
 # we may not know it at this stage
   validates :title, :presence => true
   validates :body, :presence => true
-  validates :version, :presence => true
+
+  before_create :get_note_id
+  before_create :calculate_version
+
+  def get_note_id
+    self.note_id = Note.where(:external_note_id => @external_note_id).first_or_create.id
+  end
+
+  # To calculate the version we count how many versions of this note_id already exist, then increment:
+  def calculate_version
+    self.version = NoteVersion.where(:note_id => self.note_id).count + 1
+  end
 
 end

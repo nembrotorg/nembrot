b4cc2d6309438e68bde6f8022f32d14dd23b1124
Joe Gatt <joe@joegatt.net>
2012-07-30 00:49:21 +0100

    Re-adds registrable module.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,8 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
-  # :registerable
-
-  # :registerable removed on RailsAdmin installation docs' advice
-  # May be required later
-
-  devise :database_authenticatable, :recoverable, 
-         :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me

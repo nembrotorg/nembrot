7b0deca5cd9c90f7cc37e6534ed6dc8fe1be19c8
Joe Gatt <joe@joegatt.net>
2012-08-14 19:10:37 +0100

    Tests tag instead of text.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,10 +4,17 @@ describe "Static pages" do
 
   describe "Home page" do
 
+  	before { visit '/' }
+
+    it "should have the content 'Nembrot'" do
+      page.should have_selector('h1')
+    end
+
     it "should have the content 'Nembrot'" do
       visit '/'
-      page.should have_content('Home - Nembrot')
+      page.should have_selector('h1', text: 'Home - Nembrot')
     end
+
   end
 
 end

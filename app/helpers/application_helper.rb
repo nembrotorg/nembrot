18c4e6ca5bc6073cd23a89208ad4e69b2d3e3445
Joe Gatt <joe@joegatt.net>
2012-09-01 18:43:31 +0100

    Adds helpers to pass tests.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,12 @@ module ApplicationHelper
     text = strip_tags text
     text.split[0..(wordcount-1)].join(' ') + (text.split.size > wordcount ? '...' : '') 
   end
+
+  def bodify(text)
+    (sanitize text, :tags => %w(a ul ol li), :attributes => %w(href))
+      .gsub(/^.*DOCTYPE.*>/, '')
+      .gsub(/^(\w*)$/, '')
+      .gsub(/^(.*)$/, '<p>\1</p>')
+      .html_safe
+  end
 end

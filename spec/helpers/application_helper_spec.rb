ab0b1a84d74250900c0ada41c3934c452b956b5a
Joe Gatt <joe@joegatt.net>
2012-09-01 18:43:08 +0100

    Adds tests for helpers.

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -16,4 +16,34 @@ describe ApplicationHelper do
     end
   end
 
+  describe "Bodify" do
+    it "should strip html tags" do
+      bodify("<div class='superfluous'><b>Plain text</b></div>").should == "<p>Plain text</p>"
+    end
+
+    it "should not strip links" do
+      bodify("Plain text<a href='http://example.com'>example</a>").should == "<p>Plain text<a href=\"http://example.com\">example</a></p>"
+    end
+
+    it "should not strip ordered lists" do
+      bodify("Plain text<ol><li>first</li></ol>").should == "<p>Plain text<ol><li>first</li></ol></p>"
+    end
+
+    it "should not strip unordered lists" do
+      bodify("Plain text<ul><li>first</li></ul>").should == "<p>Plain text<ul><li>first</li></ul></p>"
+    end
+
+    it "should strip all attributes (except href)" do
+      bodify("Plain text<ul class='superfluous'><li id='superfluous'>first</li></ul>").should == "<p>Plain text<ul><li>first</li></ul></p>"
+    end
+
+    it "should wrap paragraphs in <p> tags" do
+      bodify("Plain text").should == "<p>Plain text</p>"
+    end
+
+    it "should strip doctype" do
+      bodify("<!DOCTYPE html>Plain text").should == "<p>Plain text</p>"
+    end
+  end
+
 end

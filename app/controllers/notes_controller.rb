718e819397f995e31f00a208ce63199f237a8fe9
joegatt <joe@joegatt.net>
2012-11-12 00:05:28 +0100

    Skinnifies Notes controller, fattens model.

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -21,43 +21,7 @@ class NotesController < ApplicationController
 
   def version
     @note = Note.find(params[:id])
-
-    # Can this go in the model?
-
-    if params[:sequence].to_i == 1
-      @version = @note.versions.find_by_sequence(params[:sequence]).reify
-      @previous = OpenStruct.new({
-        :title => '',
-        :body => ''
-      })
-      @sequence = 1
-      version_tags = @version.version.tags
-      previous_tags = Array.new
-    elsif params[:sequence].to_i == @note.versions.length + 1
-      @version = @note
-      @previous = @note.versions.last.reify
-      @sequence = @note.versions.length + 1
-      version_tags = @version.tags
-      previous_tags = @previous.version.tags
-    else        
-      @version = @note.versions.find_by_sequence(params[:sequence]).reify
-      @previous = @version.previous_version    
-      @sequence = params[:sequence]
-      version_tags = @version.version.tags
-      previous_tags = @previous.version.tags
-    end
-
-    @undiffed_version = @version
-
-    @version.title = Differ.diff_by_word(@version.title, @previous.title)
-    @version.body = Differ.diff_by_word(@version.body, @previous.body)
-
-    @removed_tags = previous_tags - version_tags
-    @added_tags = version_tags - previous_tags
-
-    # tags default scope: sort by tag.name.downcase
-    # _tags should go in :instructions
-    @tags = (version_tags + previous_tags).find_all { |tag| tag.name.match(/^[^_]/) }.uniq.sort_by { |tag| tag.name.downcase }
+    @diffed_version = @note.diffed_version(params[:sequence].to_i)
 
     respond_to do |format|
       format.html
--- app/models/note.rb
+++ app/models/note.rb
@@ -5,14 +5,53 @@ class Note < ActiveRecord::Base
 
   acts_as_taggable
   
-	has_paper_trail :on => [:update, :destroy],
-									:meta => { 
-										:sequence  => Proc.new { |note| note.versions.length + 1 }, 
-										:tags  => Proc.new { |note| Note.find(note.id).tags } # Simply storing note.tag_list would store incoming one, so we store object
-									}
+  has_paper_trail :on => [:update, :destroy],
+                  :meta => { 
+                    :sequence  => Proc.new { |note| note.versions.length + 1 }, 
+                    :tags  => Proc.new { |note| Note.find(note.id).tags } # Simply storing note.tag_list would store incoming one, so we store object
+                  }
 
   accepts_nested_attributes_for :cloud_notes, 
-  															:reject_if => Proc.new { |a| a['cloud_note_identifier'].blank? || a['cloud_service'].blank? }
+                                :reject_if => Proc.new { |a| a['cloud_note_identifier'].blank? || a['cloud_service'].blank? }
 
   validates :title, :body, :external_updated_at, :presence => true
+
+  def headline
+    self.title + ': ' + self.body
+  end
+
+  def diffed_version(sequence)
+    if sequence == 1
+      version = self.versions.find_by_sequence(1).reify
+      previous = OpenStruct.new({
+        :title => '',
+        :body => ''
+      })
+      version_tags = version.version.tags
+      previous_tags = Array.new
+    elsif sequence == self.versions.length + 1
+      version = self
+      previous = self.versions.last.reify
+      version_tags = version.tags
+      previous_tags = previous.version.tags
+    else        
+      version = self.versions.find_by_sequence(sequence).reify
+      previous = version.previous_version    
+      version_tags = version.version.tags
+      previous_tags = previous.version.tags
+    end
+
+    OpenStruct.new({
+            :title => Differ.diff_by_word(version.title, previous.title),
+            :body => Differ.diff_by_word(version.body, previous.body),
+            :undiffed_title => version.title,
+            :undiffed_body => version.body,
+            :sequence => sequence,
+            :external_updated_at => version.external_updated_at,
+            :removed_tags => previous_tags - version_tags,
+            :added_tags => version_tags - previous_tags,
+            :tags => (version_tags + previous_tags).find_all { |tag| tag.name.match(/^[^_]/) }.uniq.sort_by { |tag| tag.name.downcase }
+          })
+  end
+
 end

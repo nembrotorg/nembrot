de51aef0ff379a65e73399d3e662c0516cca7bcb
joegatt <joe@joegatt.net>
2012-11-12 14:49:52 +0100

    Adds sort order for Tags as default_scope.

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -2,12 +2,8 @@ class TagsController < ApplicationController
   # GET /tags
   # GET /tags.json
   def index
-    # Can this be a scope in noteversions?
-    # Is this working as expected?
-    # Better to put a latest Boolean field in table then scope it  
+    # Can this be transferred to model?
     @tags = Note.tag_counts_on(:tags)
-              .find_all { |tag| tag.name.match(/^[^_]/) }
-              .sort_by { |tag| tag.name.downcase }
 
     respond_to do |format|
       format.html # index.html.erb
@@ -20,8 +16,6 @@ class TagsController < ApplicationController
   def show
     @tag = Tag.find_by_slug(params[:slug])
     @notes = Note.tagged_with(@tag.name)
-    #@notes = Note.find(:all, :order => 'updated_at DESC')
-    #@notes = Note.note_versions.tagged_with(params[:name])
 
     respond_to do |format|
       format.html # show.html.erb
--- config/initializers/acts_as_taggable_on.rb
+++ config/initializers/acts_as_taggable_on.rb
@@ -4,3 +4,7 @@ ActsAsTaggableOn::Tag.class_eval do
 	extend FriendlyId
 		friendly_id :name, use: :slugged
 end
+
+class ActsAsTaggableOn::Tag < ActiveRecord::Base
+  default_scope :order => 'name'
+end

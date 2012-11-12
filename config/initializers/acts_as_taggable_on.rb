819ea4cf49be1d97d8a6914ddb632f2e7f5c5712
joegatt <joe@joegatt.net>
2012-11-12 15:24:40 +0100

    Adds empty Tag model to allow direct manipulation.
    Although the model is maintained by the acts_as_taggable_on gem, it
    seems we need to have this here to allow us to do things like
    find_by_slug.

--- config/initializers/acts_as_taggable_on.rb
+++ config/initializers/acts_as_taggable_on.rb
@@ -1,10 +1,7 @@
 ActsAsTaggableOn.remove_unused_tags = true
 
 ActsAsTaggableOn::Tag.class_eval do
+  default_scope :order => 'name'
 	extend FriendlyId
 		friendly_id :name, use: :slugged
-end
-
-class ActsAsTaggableOn::Tag < ActiveRecord::Base
-  default_scope :order => 'name'
 end

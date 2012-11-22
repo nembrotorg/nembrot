7f8362c429746e8cc57a9591f28c70d692dd7f01
joegatt <joe@joegatt.net>
2012-11-22 08:13:37 +0100

    Improves tag regexp.

--- config/routes.rb
+++ config/routes.rb
@@ -8,15 +8,11 @@ Nembrot::Application.routes.draw do
 
   get 'notes/:id/v/:sequence' => 'notes#version', :id => /\d+/, :sequence => /\d+/
   get 'notes/:id' => 'notes#show', :id => /\d+/
-  get 'tags/:slug' => 'tags#show', :slug => /[\-\w]+/
-
-  get 'notes' => 'notes#index'
-  get 'tags' => 'tags#index'
+  get 'tags/:slug' => 'tags#show', :slug => /[a-z\d\-]+/
 
 	resources :notes, only: [:index, :show, :version] do
 		resources :v, :as => :versions
 	end
 
 	resources :tags, only: [:index, :show]
-
 end

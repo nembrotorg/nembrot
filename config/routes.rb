afe999abcf4c1b9e53405822f8eff1b3316d462f
joegatt <joe@joegatt.net>
2012-11-28 16:23:26 +0100

    Updates routes.
    Removes all default routing.

--- config/routes.rb
+++ config/routes.rb
@@ -6,13 +6,12 @@ Nembrot::Application.routes.draw do
 
 	root :to => "home#index"
 
+  get 'webhooks/evernote_note' => 'notes#update_cloud'
+
   get 'notes/:id/v/:sequence' => 'notes#version', :id => /\d+/, :sequence => /\d+/
   get 'notes/:id' => 'notes#show', :id => /\d+/
+  get 'notes' => 'notes#index'
+
   get 'tags/:slug' => 'tags#show', :slug => /[a-z\d\-]+/
-
-	resources :notes, only: [:index, :show, :version] do
-		resources :v, :as => :versions
-	end
-
-	resources :tags, only: [:index, :show]
+  get 'tags' => 'tags#index'
 end

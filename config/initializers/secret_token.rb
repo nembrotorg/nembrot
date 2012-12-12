35b9c418eb9daeb8143fded6af20b38f51b1979b
joegatt <joe@joegatt.net>
2012-12-12 15:39:52 +0100

    Expires sessions.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Nembrot::Application.config.secret_token = '6c62d4f9d487f4b79c16b9e08ee552a1e7592bc59e2162f5ec86c64a02614f7bc5b04ba6693e9384fba4d39011df830dc84604fc58a9dd4ab21ac38fba07c4c1'
+Nembrot::Application.config.secret_token = '6c62d4f9d487f7899c16b9e08ee552a1e7592bc59e2162f5ec86c64a02614f7bc5b04ba6693e9384fba4d39011df830dc84604fc58a9dd4ab21ac38fba07c4c1'

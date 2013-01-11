520a340c06d9c6a22a13793fca21fc6314d52b2f
joegatt <joe@joegatt.net>
2013-01-11 20:32:32 +0100

    Updates Rails version to 1.9.3-p362.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,7 +6,7 @@ set :application, "joegattnet_v3"
 
 default_environment["RAILS_ENV"] = 'production'
 
-default_environment["PATH"]         = "/usr/local/rvm/gems/ruby-1.9.2-p290@joegattnet_v3/bin:/usr/local/rvm/gems/ruby-1.9.2-p290@joegattnet_v3/bin:/usr/local/rvm/rubies/ruby-1.9.2-p290/bin:/usr/local/rvm/bin:$PATH"
-default_environment["GEM_HOME"]     = "/usr/local/rvm/gems/ruby-1.9.2-p290@joegattnet_v3"
-default_environment["GEM_PATH"]     = "/usr/local/rvm/gems/ruby-1.9.2-p290@joegattnet_v3:/usr/local/rvm/gems/ruby-1.9.2-p290@joegattnet_v3"
-default_environment["RUBY_VERSION"] = "ruby-1.9.2-p290"
+default_environment["PATH"]         = "/usr/local/rvm/gems/ruby-1.9.3-p362@joegattnet_v3/bin:/usr/local/rvm/gems/ruby-1.9.3-p362@joegattnet_v3/bin:/usr/local/rvm/rubies/ruby-1.9.3-p362/bin:/usr/local/rvm/bin:$PATH"
+default_environment["GEM_HOME"]     = "/usr/local/rvm/gems/ruby-1.9.3-p362@joegattnet_v3"
+default_environment["GEM_PATH"]     = "/usr/local/rvm/gems/ruby-1.9.3-p362@joegattnet_v3:/usr/local/rvm/gems/ruby-1.9.3-p362@joegattnet_v3"
+default_environment["RUBY_VERSION"] = "ruby-1.9.3-p362"

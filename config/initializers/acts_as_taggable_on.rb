4e10859af10a0e746ed94bc08d4357df758fdfff
joegatt <joe@joegatt.net>
2013-02-06 16:44:09 +0100

    Adds styling, including embedded fonts.

--- config/initializers/acts_as_taggable_on.rb
+++ config/initializers/acts_as_taggable_on.rb
@@ -3,7 +3,11 @@ ActsAsTaggableOn.remove_unused_tags = true
 ActsAsTaggableOn::Tag.class_eval do
   attr_accessor :diff_status, :obsolete
 
-  default_scope :order => 'name'
+  default_scope :order => 'slug'
+
+  def to_param
+    slug
+  end
 
 	extend FriendlyId
 		friendly_id :name, use: :slugged

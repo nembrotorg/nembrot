69eee4bd8d5ad7525b8a713526c88df78ff84108
joegatt <joe@joegatt.net>
2013-02-07 18:21:20 +0100

    Adds tags index selection.

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -3,12 +3,12 @@ class TagsController < ApplicationController
   add_breadcrumb I18n.t('tags.title'), :tags_path
 
   def index
-    if Settings.tags.cloud
+    if Settings.tags.index.style == 'cloud'
       @tags = Note.tag_counts_on(:tags)
     else
       @tags = Note.tag_counts
     end
-    
+
     respond_to do |format|
       format.html
       format.json { render :json => @tags }
@@ -18,6 +18,12 @@ class TagsController < ApplicationController
   def show
     @tag = Tag.find_by_slug(params[:slug])
     @notes = Note.tagged_with(@tag.name)
+
+    if Settings.tags.index.style == 'cloud'
+      @tags = Note.tag_counts_on(:tags)
+    else
+      @tags = Note.tag_counts
+    end
 
     add_breadcrumb @tag.name, tag_path(params[:slug])
 

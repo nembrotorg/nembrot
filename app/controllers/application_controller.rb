b347ea2da94a05bb82012312d7403d3841be0d6f
joegatt <joe@joegatt.net>
2013-02-08 10:09:43 +0100

    Fixes translation missing for home breadcrumb.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,14 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery
 
-  add_breadcrumb I18n.t('home.title'), :root_path
-
   before_filter :set_locale
 
   def set_locale
-    I18n.locale = params[:locale] || Settings.locale
+    I18n.locale = params[:locale] || Settings.locale || I18n.default_locale
+
+    # Strange place for this but otherwise 'home' translatin is not read.
+    # See http://stackoverflow.com/questions/10805196/ruby-on-rails-i18n-breadcumbs
+    # And: http://stackoverflow.com/questions/10865962/rails-breadcrumb-and-i18n
+    add_breadcrumb I18n.t('home.title'), :root_path
   end
 end

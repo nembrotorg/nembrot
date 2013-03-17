b8a6345bc4c4f4735a73cb9e22556b0b05c4f969
joegatt <joe@joegatt.net>
2013-03-17 13:37:11 +0100

    Fixes RSpec threadsafe issues.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -123,6 +123,19 @@ Spork.prefork do
       #   end
       # end
     end
+
+    # This is a provisional fix for an rspec-rails issue
+    # (see https://github.com/rspec/rspec-rails/issues/476).
+    # It allows for a proper test execution with `config.threadsafe!`.
+    ActionView::TestCase::TestController.instance_eval do
+      helper Rails.application.routes.url_helpers#, (append other helpers you need)
+    end
+    ActionView::TestCase::TestController.class_eval do
+      def _routes
+        Rails.application.routes
+      end
+    end
+
   end
 
   # Find files to put into preload

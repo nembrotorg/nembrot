9fbd110dcbd7b92793ef6d5b3afd800ea628c76f
joegatt <joe@joegatt.net>
2013-06-03 17:03:38 +0200

    Improves tests for GoogleBooks API calls.

--- app/models/google_books.rb
+++ app/models/google_books.rb
@@ -1,18 +1,28 @@
+# encoding: utf-8
+
 class GoogleBooks
   include HTTParty
 
   base_uri Settings.books.google_books.domain
 
-   attr_accessor :isbn, :title, :author, :lang, :published_date, :isbn_10, :isbn_13, :page_count, :google_books_id, :response
+  attr_accessor :isbn, :title, :author, :lang, :published_date, :isbn_10, :isbn_13, :page_count, :google_books_id,
+                :response
 
-   def initialize(isbn)
-
+  def initialize(isbn)
     @isbn = isbn
 
     params = { 'country' => 'GB', 'q' => "ISBN:#{ isbn }" }
-    response = self.class.get(Settings.books.google_books.path, :query => params)
+    response = self.class.get(Settings.books.google_books.path, query: params)
 
-    if response && response['items'].first
+    assign_values(response) if response && response['items'].first
+
+    rescue => error
+      puts "Error while fetching #{ isbn } from GoogleBooks."
+      puts error
+      puts error.backtrace
+  end
+
+  def assign_values(response)
       response = response['items'].first
       volume_info = response['volumeInfo']
 
@@ -23,10 +33,5 @@ class GoogleBooks
       @published_date = volume_info['publishedDate']
       @page_count = volume_info['pageCount']
       @google_books_id = response['id']
-    end
-    rescue => error
-      puts "Error while fetching #{ isbn } from GoogleBooks."
-      puts error
-      puts error.backtrace
   end
 end
--- spec/models/google_books_spec.rb
+++ spec/models/google_books_spec.rb
@@ -1,13 +1,20 @@
+# encoding: utf-8
+
 describe GoogleBooks do
 
-  before {
-    VCR.use_cassette('model/google_books', :decode_compressed_response => true) do
+  before do
+    VCR.use_cassette('model/google_books', decode_compressed_response: true) do
       @google_book = GoogleBooks.new('0123456789')
     end
-  }
+  end
 
   subject { @google_book }
 
   its (:title) { should == 'The Man Without Qualities' }
+  its (:author) { should == 'Professor Robert Musil' }
+  its (:lang) { should == 'en' }
+  its (:published_date) { should == '2011-09-16' }
+  its (:page_count) { should == 1130 }
+  its (:google_books_id) { should == 'EmQ6ygAACAAJ' }
 
 end

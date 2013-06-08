07d1ea4165dc3684965217e7cc40856cd5944096
joegatt <joe@joegatt.net>
2013-06-08 16:22:50 +0200

    Converts <b> to <h2> when displaying a note.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,8 @@
+# encoding: utf-8
+
 module ApplicationHelper
   def lang_attr(language)
-    if language != I18n.locale.to_s
-      language
-    end
+    language if language != I18n.locale.to_s
   end
 
   def body_dir_attr(language)
@@ -13,30 +13,27 @@ module ApplicationHelper
     if language != I18n.locale.to_s
       page_direction = Settings.lang.rtl_langs.include?(I18n.locale.to_s) ? 'rtl' : 'ltr'
       this_direction = Settings.lang.rtl_langs.include?(language) ? 'rtl' : 'ltr'
-      if page_direction != this_direction
-        this_direction
-      end
+      this_direction if page_direction != this_direction
     end
   end
 
   def snippet(text, characters, omission = '...')
-    text = ActionController::Base.helpers.sanitize(text, :tags => ['h2'])
+    text = ActionController::Base.helpers.sanitize(text, tags: ['h2'])
     text = text.gsub(/\[.+\]/, '')
-    text = ActionController::Base.helpers.truncate(text, :length => characters, :separator => ' ', :omission => omission)
+    text = ActionController::Base.helpers.truncate(text, length: characters, separator: ' ', omission: omission)
   end
 
   def smartify(text)
-    text
-      .gsub(/ - ([^-^.]+) - /, "\u2013\\1\u2013")
-      .gsub(/  /, " ")
-      .gsub(/ - /, "\u2014")
-      .gsub(/'([\d{2}])/, "\u2019\\1")
-      .gsub(/(\b)'(\b)/, "\u2019")
-      .gsub(/(\w)'(\w)/, "\\1\u2019\\2")
-      .gsub(/'([^']+)'/, "\u2018\\1\u2019")
-      .gsub(/"([^"]+)"/, "\u201C\\1\u201D")
-      .gsub(/(<[^>]*)\u201C([a-zA-Z0-9\.\-\/:_]+?)\u201D([^<]*\>)/, "\\1\"\\2\"\\3")
-      .gsub(/(\d)\^([\d\,\.]+)/, "\\1<sup>\\2</sup>")
+    text.gsub(/ - ([^-^.]+) - /, '\u2013\\1\u2013')
+        .gsub(/  /, ' ')
+        .gsub(/ - /, '\u2014')
+        .gsub(/'([\d{2}])/, '\u2019\\1')
+        .gsub(/(\b)'(\b)/, '\u2019')
+        .gsub(/(\w)'(\w)/, '\\1\u2019\\2')
+        .gsub(/'([^']+)'/, '\u2018\\1\u2019')
+        .gsub(/"([^"]+)"/, '\u201C\\1\u201D')
+        .gsub(/(<[^>]*)\u201C([a-zA-Z0-9\.\-\/:_]+?)\u201D([^<]*\>)/, '\\1\'\\2\'\\3')
+        .gsub(/(\d)\^([\d\,\.]+)/, '\\1<sup>\\2</sup>')
   end
 
   def notify(text)
@@ -49,14 +46,14 @@ module ApplicationHelper
     text = bookify(text, books)
     text = smartify(text)
     text = notify(text)
-    text = text
-      .strip
-      .gsub(/[\n]+/, "\n")
-      .gsub(/^([^<].+[^>])$/, '<p>\1</p>')
-      .gsub(/^<strong>(.+)<\/strong>$/, '<h2>\1</h2>')
-      .gsub(/^(<p> *<\/p)>$/, '')
-      .gsub(/^ +$/, '')
-      .html_safe
+    text = text.strip
+               .gsub(/[\n]+/, '\n')
+               .gsub(/^([^<].+[^>])$/, '<p>\1</p>')
+               .gsub(/^<strong>(.+)<\/strong>$/, '<h2>\1</h2>')
+               .gsub(/^<b>(.+)<\/b>$/, '<h2>\1</h2>')
+               .gsub(/^(<p> *<\/p)>$/, '')
+               .gsub(/^ +$/, '')
+               .html_safe
   end
 
   def bookify(text, books)

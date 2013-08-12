0fc168bc37456f0d60fcd69d5c370b09c9ba6d08
joegatt <joe@joegatt.net>
2013-08-12 19:14:05 +0200

    Remove log message for every run

--- script/daemons/sync_all.rb
+++ script/daemons/sync_all.rb
@@ -3,7 +3,6 @@ require File.expand_path(File.join(File.dirname(__FILE__), '../..', 'config', 'e
 require 'rubygems'
 
 loop do
-  puts "Fetching at #{ Time.now.to_s(:time) }..."
   EvernoteNote.sync_all
   Resource.sync_all_binaries
   Book.sync_all

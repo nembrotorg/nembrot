541cd0a3cbc5787d8d294240c7fa6b57a39e8863
joegatt <joe@joegatt.net>
2013-08-30 08:55:41 +0200

    Move assets version to an initializer
    This makes it easier to expire all assets.

--- config/application.rb
+++ config/application.rb
@@ -48,9 +48,6 @@ module Nembrot
     # Enable the asset pipeline
     config.assets.enabled = true
 
-    # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.1'
-
     # Generators
     config.generators do |g|
       g.test_framework :rspec,

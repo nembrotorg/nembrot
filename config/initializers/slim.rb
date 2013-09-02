392b0c760d5f9928ec38cd5a871c87827eea4a1d
joegatt <joe@joegatt.net>
2013-09-02 22:11:55 +0200

    Fix typo

--- config/initializers/slim.rb
+++ config/initializers/slim.rb
@@ -2,4 +2,4 @@
 Slim::Engine.set_default_options format: Settings.html.format.to_sym,
                                  pretty: Settings.html.pretty,
                                  sort_attrs: Settings.html.sort_attrs,
-                                 tab_size: Settings.html.tab_size
+                                 tabsize: Settings.html.tabsize

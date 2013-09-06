1eafb474ddeac377721ebf7a8bae5a010ffa45a2
joegatt <joe@joegatt.net>
2013-09-06 19:19:45 +0200

    Hardcode schedule times

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,12 +1,13 @@
 # See https://github.com/javan/whenever/pull/332
 job_type :runner, "cd :path && bin/rails runner -e :environment ':task' :output"
-
-require File.expand_path(File.join(File.dirname(__FILE__), '..', 'config', 'environment'))
-require 'rubygems'
-
 set :output, "#{ Whenever.path }/log/sync.log"
 
-every Settings.notes.synch_every_minutes.minutes do
+# REVIEW:
+#  require File.expand_path(File.join(File.dirname(__FILE__), '..', 'config', 'environment'))
+#  require 'rubygems'
+#  every Settings.notes.synch_every_minutes.minutes do
+
+every 1.minute do
   runner 'EvernoteNote.sync_all'
   runner 'Resource.sync_all_binaries'
   runner 'Book.sync_all'

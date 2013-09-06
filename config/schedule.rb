7b413f213c5227cc901f8c91dfe911bc4cc1db20
joegatt <joe@joegatt.net>
2013-09-06 18:54:31 +0200

    Add custom runner

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,23 +1,5 @@
-# Use this file to easily define all of your cron jobs.
-#
-# It's helpful, but not entirely necessary to understand cron before proceeding.
-# http://en.wikipedia.org/wiki/Cron
-
-# Example:
-#
-# set :output, "/path/to/my/cron_log.log"
-#
-# every 2.hours do
-#   command "/usr/bin/some_great_command"
-#   runner "MyModel.some_method"
-#   rake "some:great:rake:task"
-# end
-#
-# every 4.days do
-#   runner "AnotherModel.prune_old_records"
-# end
-
-# Learn more: http://github.com/javan/whenever
+# See https://github.com/javan/whenever/pull/332
+job_type :runner, "cd :path && bin/rails runner -e :environment ':task' :output"
 
 require File.expand_path(File.join(File.dirname(__FILE__), '..', 'config', 'environment'))
 require 'rubygems'

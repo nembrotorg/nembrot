386a8492023d49547a50f97e55d153e42f15a60d
joegatt <joe@joegatt.net>
2013-09-08 21:26:51 +0200

    Superfluous

--- db/migrate/20130908115503_rename_pantographers_twitter_user_name_twitter_screen_name.rb
+++
@@ -1,5 +0,0 @@
-class RenamePantographersTwitterUserNameTwitterScreenName < ActiveRecord::Migration
-  def change
-    rename_column :pantographers, :twitter_screen_name, :twitter_screen_name
-  end
-end

5bd78869e242032c3ddc2f2ca86047abfa6e4ee6
joegatt <joe@joegatt.net>
2013-09-29 00:43:45 +0200

    Add sitemap (for search engines)

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,10 +1,14 @@
 set :output, "#{ path }/log/sync.log"
 
-# REVIEW:
+# REVIEW: get times from Settings
 #  require File.expand_path(File.join(File.dirname(__FILE__), '..', 'config', 'environment'))
 #  require 'rubygems'
 #  every Settings.notes.synch_every_minutes.minutes do
 
 every 1.minute do
   rake 'sync:all'
+end
+
+every 1.day, at: '5:00 am' do
+  rake '-s sitemap:refresh'
 end

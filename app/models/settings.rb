e55817ad1b6e3ee5becbc39fa41b3ffc2ae4b8ec
joegatt <joe@joegatt.net>
2013-09-28 21:44:01 +0200

    Read settings from multiple files
    All /config/*.settings.yml files are read.

--- app/models/settings.rb
+++ app/models/settings.rb
@@ -1,4 +1,8 @@
 class Settings < Settingslogic
   source "#{ Rails.root }/config/settings.yml"
   namespace Rails.env
+
+  Dir.glob("#{ Rails.root }/config/*.settings.yml") do |extra_settings_file|
+    instance.deep_merge!(Settings.new(extra_settings_file))
+  end
 end

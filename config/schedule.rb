0f6ce754cc689d1cf8095ac802502da4b7ca10c4
joegatt <joe@joegatt.net>
2013-10-03 20:20:22 +0200

    Add sitemap config (but comment out schedule)

--- config/schedule.rb
+++ config/schedule.rb
@@ -17,6 +17,7 @@ every '0 * * * *' do
   rake 'joegattnet:one_hour'
 end
 
-every 1.day, at: '5:00 am' do
-  rake '-s sitemap:refresh'
-end
+# Disabled until launch
+# every 1.day, at: '5:00 am' do
+#   rake '-s sitemap:refresh'
+# end
---
+++ config/sitemap.rb
@@ -0,0 +1,41 @@
+SitemapGenerator::Sitemap.default_host = "http://#{ Settings.host }"
+
+SitemapGenerator::Sitemap.create do
+
+  # Defaults: :priority => 0.5, :changefreq => 'weekly',
+  #           :lastmod => Time.now, :host => default_host
+
+  unless Book.cited.all.empty?
+    add books_path
+
+    Book.cited.each do |book|
+      add book_path(book), lastmod: book.updated_at
+    end
+  end
+
+  unless Link.publishable.all.empty?
+    add links_path
+
+    Link.publishable.each do |link|
+      add link_path(link), lastmod: link.updated_at
+    end
+  end
+
+  unless Note.publishable.listable.all.empty?
+    add notes_path
+
+    Note.publishable.listable.each do |note|
+      add note_path(note), lastmod: note.external_updated_at
+    end
+  end
+
+  unless Note.publishable.tag_counts_on(:tags).empty?
+    add tags_path
+
+    Note.publishable.tag_counts_on(:tags).each do |tag|
+      add tag_path(tag), lastmod: Time.now
+    end
+  end
+
+  add pantographs_path, lastmod: Time.now
+end

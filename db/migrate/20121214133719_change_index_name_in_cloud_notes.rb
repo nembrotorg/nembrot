fdf01b1e33be21e0a2e7b958f815f86ee5d4a2f8
joegatt <joe@joegatt.net>
2013-10-09 19:04:00 +0200

    Skip migration

--- db/migrate/20121214133719_change_index_name_in_cloud_notes.rb
+++
@@ -1,9 +0,0 @@
-# class ChangeIndexNameInCloudNotes < ActiveRecord::Migration
-#   def up
-#     rename_index :cloud_notes, 'index_cloud_notes_on_cloud_note_identifier_and_cloud_service_id', 'index_cloud_notes_on_identifier_service_id'
-#   end
-
-#   def down
-#     rename_index :cloud_notes, 'index_cloud_notes_on_identifier_service_id', 'index_cloud_notes_on_cloud_note_identifier_and_cloud_service_id'
-#   end
-# end

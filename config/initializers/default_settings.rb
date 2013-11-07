cc2427f9e7546e118cc19bd56f5cd15960fcb712
joegatt <joe@joegatt.net>
2013-11-07 18:41:08 +0100

    Delay settings initialisation until after db:migrate
    Otherwise it crashes in Travis CI.

--- config/initializers/default_settings.rb
+++ config/initializers/default_settings.rb
@@ -1,9 +1,15 @@
-Constant.channel.map do |key, value|
-  # Namespaced defaults don't work in rails-settings-cached gem
-  # Setting.save_default("channel.#{ key }", value)
-  Setting["channel.#{ key }"] = value if Setting["channel.#{ key }"].blank?
-end
+# See http://stackoverflow.com/questions/19841292
 
-Constant.style.map do |key, value|
-  Setting["style.#{ key }"] = value if Setting["style.#{ key }"].blank?
+if ActiveRecord::Base.connection.table_exists? 'settings'
+
+  Constant.channel.map do |key, value|
+    # Try Namespaced defaults
+    # Setting.save_default("channel.#{ key }", value)
+    Setting["channel.#{ key }"] = value if Setting["channel.#{ key }"].blank?
+  end
+
+  Constant.style.map do |key, value|
+    Setting["style.#{ key }"] = value if Setting["style.#{ key }"].blank?
+  end
+
 end

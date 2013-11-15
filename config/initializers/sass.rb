e396203ab81a0f88ddb8d07a98ed4f732a2d4777
joegatt <joe@joegatt.net>
2013-11-15 11:22:07 +0100

    Skip settings if table has not been created

--- config/initializers/sass.rb
+++ config/initializers/sass.rb
@@ -3,7 +3,8 @@
 module Sass::Script::Functions
   def settings_style(setting)
     assert_type setting, :String
-    Sass::Script::Parser.parse(Setting["#{ setting.value }"].to_s, 0, 0)
+    return_value = ActiveRecord::Base.connection.table_exists? 'settings' ? Setting["#{ setting.value }"].to_s : 'NOT_SET'
+    Sass::Script::Parser.parse(return_value, 0, 0)
   end
   declare :settings_style, args: [:setting]
 end

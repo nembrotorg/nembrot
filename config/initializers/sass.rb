22c691553747b10562b51246ac78b65e5453a1d9
joegatt <joe@joegatt.net>
2013-11-15 13:45:37 +0100

    Fix sass function

--- config/initializers/sass.rb
+++ config/initializers/sass.rb
@@ -3,8 +3,8 @@
 module Sass::Script::Functions
   def settings_style(setting)
     assert_type setting, :String
-    return_value = ActiveRecord::Base.connection.table_exists?('settings') ? Setting["#{ setting.value }"].to_s : Constant.style["#{ setting.value }".gsub(/^style\./, '')]
-    Sass::Script::Parser.parse(return_value, 0, 0)
+    return_value = (ActiveRecord::Base.connection.table_exists?('settings') ? Setting["#{ setting.value }"] : Constant.style["#{ setting.value }".gsub(/^style\./, '')])
+    Sass::Script::Parser.parse(return_value.to_s, 0, 0)
   end
   declare :settings_style, args: [:setting]
 end

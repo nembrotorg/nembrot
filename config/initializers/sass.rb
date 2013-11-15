f499be8464ec470edfdaa033dd23af796bbbeca8
joegatt <joe@joegatt.net>
2013-11-15 12:25:30 +0100

    Fix default setting for styles

--- config/initializers/sass.rb
+++ config/initializers/sass.rb
@@ -3,7 +3,7 @@
 module Sass::Script::Functions
   def settings_style(setting)
     assert_type setting, :String
-    return_value = ActiveRecord::Base.connection.table_exists?('settings') ? Setting["#{ setting.value }"].to_s : Constant["#{ setting.value }"].to_s
+    return_value = ActiveRecord::Base.connection.table_exists?('settings') ? Setting["#{ setting.value }"].to_s : Constant["#{ setting.value }"]
     Sass::Script::Parser.parse(return_value, 0, 0)
   end
   declare :settings_style, args: [:setting]

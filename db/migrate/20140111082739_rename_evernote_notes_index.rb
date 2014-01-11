f102a5b2cd27b777c237157fdbd2948fec4ae07b
joegatt <joe@joegatt.net>
2014-01-11 10:20:20 +0100

    Delete superfluous migration

--- db/migrate/20140111082739_rename_evernote_notes_index.rb
+++
@@ -1,5 +0,0 @@
-class RenameEvernoteNotesIndex < ActiveRecord::Migration
-  def change
-    rename_index :evernote_notes, :index_cloud_notes_on_cloud_note_id_and_cloud_service_id, :index_cloud_notes_on_cloud_note_identifier
-  end
-end

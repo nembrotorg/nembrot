9f66ddc6f4ff925d2ec3323d6612ac49d33d1ea0
joegatt <joe@joegatt.net>
2014-02-05 21:23:50 +0100

    Update sitemap configuration

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -2,50 +2,40 @@ SitemapGenerator::Sitemap.default_host = "http://#{ Constant.host }"
 
 SitemapGenerator::Sitemap.create do
 
-  unless Book.cited.empty?
-    add books_path
 
-    Book.cited.each do |book|
-      add book_path(book), lastmod: book.updated_at
-    end
-  end
+  Channel.where.not(slug: 'default').each do |channel|
 
-  unless Link.publishable.empty?
-    add links_path
+    add home_path(channel)
 
-    Link.publishable.each do |link|
-      add link_path(link), lastmod: link.updated_at
-    end
-  end
+    unless Note.channelled(channel).publishable.listable.empty?
+      add notes_path(channel)
 
-  unless Note.publishable.listable.empty?
-    add notes_path
+      Note.channelled(channel).publishable.listable.each do |note|
+        # REVIEW: Unable to include ApplicationHelper here
+        # Feaure and section indices are added later
+        if note.feature.nil? || !note.feature_id.nil?
+          add (note.has_instruction?('feature') ? feature_path(channel: channel.slug, feature: note.feature, feature_id: note.feature_id) : note_path(channel: channel.slug, id: note.id)),
+           lastmod: note.external_updated_at,
+           priority: (note.is_feature? && !note.is_section?) ? 0.8 : 0.7
+        end
+      end
 
-    Note.publishable.listable.each do |note|
-      # REVIEW: Unable to include ApplicationHelper here
-      # Feaure and section indices are added later
-      if note.feature.nil? || !note.feature_id.nil?
-        add (note.has_instruction?('feature') ? feature_path(note.feature, note.feature_id) : note_path(note)), 
-         lastmod: note.external_updated_at,
-         priority: (note.is_feature? && !note.is_section?) ? 0.8 : 0.7
+      Note.channelled(channel).features.each do |feature|
+        add feature_path(channel: channel.slug, feature: feature), priority: 0.9
+      end
+
+      # REVIEW: Lastmod here becomes more recent than anything else
+      Note.channelled(channel).sections.each do |section|
+        add feature_path(channel: channel.slug, feature: section)
       end
     end
 
-    Note.features.each do |feature|
-      add feature_path(feature: feature), priority: 0.9
-    end
+    unless Note.channelled(channel).publishable.tag_counts_on(:tags).empty?
+      add tags_path(channel)
 
-    # REVIEW: Lastmod here becomes more recent than anything else
-    Note.sections.each do |section|
-      add feature_path(feature: section)
-    end
-  end
-
-  unless Note.publishable.tag_counts_on(:tags).empty?
-    add tags_path
-
-    Note.publishable.tag_counts_on(:tags).each do |tag|
-      add tag_path(tag), lastmod: Time.now
+      Note.channelled(channel).publishable.tag_counts_on(:tags).each do |tag|
+        add tag_path(channel: channel.slug, slug: tag.slug), lastmod: Time.now
+      end
     end
   end
 end

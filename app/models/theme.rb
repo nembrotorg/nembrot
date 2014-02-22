944f3ca396a81111ca89bc9adef1c2e593ae9ef2
joegatt <joe@joegatt.net>
2014-02-22 17:33:55 +0100

    Slug is written automatically

--- app/models/theme.rb
+++ app/models/theme.rb
@@ -4,13 +4,21 @@ class Theme < ActiveRecord::Base
 
   scope :public, -> { where(public: true) }
 
+  before_validation :update_slug
+
   def self.hash_for_js
     #REVIEW: This can be more efficient
     themes_hash = Hash.new
 
     all.each do |t|
-      themes_hash[t.slug] = { typekit_code: t.typekit_code, effects: t.effects, map_style: t.map_style }
+      themes_hash[t.slug] = { typekit_code: t.typekit_code, effects: t.effects, map_style: t.map_style, css: t.css }
     end
     themes_hash
+  end
+
+  private
+
+  def update_slug
+    self.slug = name.parameterize
   end
 end

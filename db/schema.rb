d5027130fb61fd634ef7dd99ce90d98eadcf6143
joegatt <joe@joegatt.net>
2014-02-24 20:55:08 +0100

    Change string columns to text in Resources table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140222151426) do
+ActiveRecord::Schema.define(version: 20140224195121) do
 
   create_table "authorizations", force: true do |t|
     t.string   "provider"
@@ -210,9 +210,9 @@ ActiveRecord::Schema.define(version: 20140222151426) do
   create_table "resources", force: true do |t|
     t.string   "cloud_resource_identifier"
     t.string   "mime"
-    t.string   "caption"
-    t.string   "description"
-    t.string   "credit"
+    t.text     "caption",                   limit: 255
+    t.text     "description",               limit: 255
+    t.text     "credit",                    limit: 255
     t.string   "source_url"
     t.datetime "external_updated_at"
     t.float    "latitude"

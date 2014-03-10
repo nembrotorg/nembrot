7d323a42525fc46b0425a7e46ea37e5bf90c7c0f
joegatt <joe@joegatt.net>
2014-03-10 23:03:41 +0100

    Turn on SASS debug mode

--- config/compass.rb
+++ config/compass.rb
@@ -19,3 +19,5 @@ http_path = "/"
 preferred_syntax = :sass
 # and then run:
 # sass-convert -R --from scss --to sass app/stylesheets scss && rm -rf sass && mv scss sass
+
+sass_options = { :debug_info => true }

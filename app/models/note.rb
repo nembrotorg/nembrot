381e49ede6f82d6fb057bef066b3101d28102aba
joegatt <joe@joegatt.net>
2014-03-13 18:09:22 +0100

    Fix setting conditionals
    All settings are stored as strings.

--- app/models/note.rb
+++ app/models/note.rb
@@ -25,7 +25,7 @@ class Note < ActiveRecord::Base
   has_paper_trail on: [:update],
                   only: [:title, :body],
                   if:  proc { |note| note.save_new_version? },
-                  unless: proc { |note| Setting['advanced.versions'] == false || note.has_instruction?('reset') || note.has_instruction?('unversion') },
+                  unless: proc { |note| Setting['advanced.versions'] == 'false' || note.has_instruction?('reset') || note.has_instruction?('unversion') },
                   meta: {
                     external_updated_at: proc { |note| Note.find(note.id).external_updated_at },
                     instruction_list: proc { |note| Note.find(note.id).instruction_list },

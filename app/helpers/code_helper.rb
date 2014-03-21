b6368c6377a85c8784afde719b602b6eb4c5ea63
joegatt <joe@joegatt.net>
2014-03-22 00:40:37 +0100

    Refactor code section

---
+++ app/helpers/code_helper.rb
@@ -0,0 +1,19 @@
+# encoding: utf-8
+
+module CodeHelper
+  def model_file(controller)
+    File.join('app', 'models', "#{ controller.singularize }.rb")
+  end
+
+  def controller_file(controller)
+    File.join('app', 'controllers', "#{ controller.pluralize }_controller.rb")
+  end
+
+  def view_file(controller, action)
+    File.join('app', 'views', "#{ controller.pluralize }/#{ action }.html.slim")
+  end
+
+  def coderay(file)
+    CodeRay.scan(File.read(File.join(Rails.root, file)), :ruby).html(:line_numbers => :table, :tab_width => 2)
+  end
+end

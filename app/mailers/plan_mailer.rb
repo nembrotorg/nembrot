f73f47606e33d6d38e927cc45b14c9a36cd349a1
joegatt <joe@joegatt.net>
2014-06-21 13:05:26 +0200

    Add downgrade emails

--- app/mailers/plan_mailer.rb
+++ app/mailers/plan_mailer.rb
@@ -3,45 +3,38 @@ class PlanMailer < ActionMailer::Base
 
   def successful_upgrade(user)
     @first_name = user.first_name
-    @email = user.email
+    @last_name = user.last_name
+    @email = user.paypal_email
     @plan = user.plan.name
 
     mail(
-      to: @email,
-      subject: 'Upgraded to Premium user on nembrot.com'
-    )
-  end
-
-  def unconfirmed_upgrade(user)
-    @first_name = user.first_name
-    @email = user.email
-    @plan = user.plan.name
-
-    mail(
-      to: @email,
-      subject: 'Unsuccessful upgrade on nembrot.com'
+      to: "#{ @first_name } #{ @last_name } <#{ @email }>",
+      subject: "Upgraded to #{ @plan } user on nembrot.com"
     )
   end
 
   def downgrade_warning(user)
     @first_name = user.first_name
-    @email = user.email
+    @email = user.paypal_email
     @plan = user.plan.name
+    @free_plan = Plan.free.name
 
     mail(
-      to: @email,
-      subject: 'Your subscription to nembrot.com has not been renewed'
+      to: "#{ @first_name } #{ @last_name } <#{ @email }>",
+      subject: "Your subscription payment for nembrot.com #{ @plan } has not been received"
     )
   end
 
   def downgrade(user)
     @first_name = user.first_name
-    @email = user.email
+    @email = user.paypal_email
     @plan = user.plan.name
+    @free_plan = Plan.free.name
+    @max_free_channels = Plan.free.max_channels
 
     mail(
-      to: @email,
-      subject: 'Your nembrot.com account will be reset to Free'
+      to: "#{ @first_name } #{ @last_name } <#{ @email }>",
+      subject: "Your nembrot.com account has been reset to #{ @free_plan }"
     )
   end
 end

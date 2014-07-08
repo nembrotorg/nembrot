e4b207ad86020b73f1ff113411b75bd48a59b17d
joegatt <joe@joegatt.net>
2014-07-08 22:20:06 +0200

    Add ad hoc curtain

--- app/helpers/page_helper.rb
+++ app/helpers/page_helper.rb
@@ -3,7 +3,7 @@
 module PageHelper
   def page_wrapper(wrapper = 'section', css_list = [], &block)
     <<-EOS
-    <#{ wrapper } class="theme-#{ @current_channel.theme.slug } #{ controller.controller_name }-#{ controller.action_name } #{ @current_channel_css_modules } #{ css_instructions(css_list) unless css_list.blank? }"  data-theme-wrapper="true" data-channel-name="#{ @current_channel.name }" data-channel-id="#{ @current_channel.id }" data-channel-slug="#{ @current_channel.slug }" data-controller="#{ controller.controller_name }" data-action="#{ controller.action_name }" data-theme="#{ @current_channel.theme.slug }">
+    <#{ wrapper } class="theme-#{ @current_channel.theme.slug } #{ controller.controller_name }-#{ controller.action_name } #{ @current_channel_css_modules } #{ css_instructions(css_list).join(' ') unless css_list.blank? }"  data-theme-wrapper="true" data-channel-name="#{ @current_channel.name }" data-channel-id="#{ @current_channel.id }" data-channel-slug="#{ @current_channel.slug }" data-controller="#{ controller.controller_name }" data-action="#{ controller.action_name }" data-theme="#{ @current_channel.theme.slug }">
       #{ render 'channels/top_menu' if @current_channel.menu_at_top? }
       #{ '<nav>' + (render_breadcrumbs builder: ::OrderedListBuilder) + '</nav>' if @current_channel.breadcrumbs? }
       #{ capture(&block) }

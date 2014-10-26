0b1f320cfc5919865da2c8a6bf3b0b39956cf333
joegatt <joe@joegatt.net>
2014-10-26 19:46:30 +0100

    Let theme determine menu + breadcrumbs

--- app/helpers/page_helper.rb
+++ app/helpers/page_helper.rb
@@ -4,10 +4,10 @@ module PageHelper
   def page_wrapper(wrapper = 'section', css_list = [], &block)
     <<-EOS
     <#{ wrapper } class="theme-#{ @current_channel.theme.slug } #{ controller.controller_name }-#{ controller.action_name } #{ @current_channel_css_modules } #{ css_instructions(css_list).join(' ') unless css_list.blank? } #{ @special_css_instructions unless @special_css_instructions.blank? }"  data-theme-wrapper="true" data-channel-name="#{ @current_channel.name }" data-channel-id="#{ @current_channel.id }" data-channel-slug="#{ @current_channel.slug }" data-controller="#{ controller.controller_name }" data-action="#{ controller.action_name }" data-theme="#{ @current_channel.theme.slug }">
-      #{ render 'channels/top_menu' if @current_channel.menu_at_top? }
+      #{ render 'channels/top_menu' unless @current_channel.nil? || @current_channel == @default_channel }
       #{ '<nav>' + (render_breadcrumbs builder: ::OrderedListBuilder) + '</nav>' if @current_channel.breadcrumbs? }
       #{ capture(&block) }
-      #{ render 'channels/footer' unless @current_channel.nil? || !@current_channel.menu_at_bottom? || @current_channel == @default_channel }
+      #{ render 'channels/footer' unless @current_channel.nil? || @current_channel == @default_channel }
       #{ render 'shared/map_script', map: @map_all_markers, key: 'MAP_ALL_MARKERS' unless @map_all_markers.blank? }
     </#{ wrapper }>
     EOS

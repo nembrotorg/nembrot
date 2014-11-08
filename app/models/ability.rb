3b0ffcc93b5d0fa1093d39d560fe5430d24beee3
joegatt <joe@joegatt.net>
2014-11-08 19:31:21 +0100

    Add access permission for /themes/hash.js

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -8,12 +8,14 @@ class Ability
       can :manage, :all
     elsif !user.confirmed_at.nil? # REVIEW: See http://stackoverflow.com/questions/17126490/devise-user-signed-in-not-accessible-from-cancan-ability-model
       can :read, :all
+      can :hash, Theme
       can :show_channel, Link
       can :menu, User
       can :available, Channel
       can :manage, Channel, :user_id => user.id
     else
       can :read, :all
+      can :hash, Theme
       can :show_channel, Link
       can :menu, User
       can :create, Channel

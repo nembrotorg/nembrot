073b1afa25f54b1ff936cbdc80bcd06926a41750
joegatt <joe@joegatt.net>
2014-11-08 20:37:35 +0100

    Fix variable reference

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -6,13 +6,13 @@ class Ability
 
     if current_user.admin?
       can :manage, :all
-    elsif !user.confirmed_at.nil? # REVIEW: See http://stackoverflow.com/questions/17126490/devise-user-signed-in-not-accessible-from-cancan-ability-model
+    elsif !current_user.confirmed_at.nil? # REVIEW: See http://stackoverflow.com/questions/17126490/devise-user-signed-in-not-accessible-from-cancan-ability-model
       can :read, :all
       can :hash, Theme
       can :show_channel, Link
       can :menu, User
       can :available, Channel
-      can :manage, Channel, :user_id => user.id
+      can :manage, Channel, :user_id => current_user.id
     else
       can :read, :all
       can :hash, Theme

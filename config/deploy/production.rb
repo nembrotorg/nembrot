b915cf192e6b7125e37a6301e7efd3921c32bc9a
joegatt <joe@joegatt.net>
2015-03-17 23:21:12 +0100

    Fix deploy locations

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,12 +1,12 @@
 set :branch,      'origin/master'
 set :rails_env,   'production'
-set :deploy_to,   '/home/deployer/apps/nembrotcom'
+set :deploy_to,   '/home/deployer/apps/nembrot'
 
-set :application, 'nembrotcom'
+set :application, 'nembrot'
 
 default_environment['RAILS_ENV'] = 'production'
 
-default_environment['PATH']         = '/usr/local/rvm/gems/ruby-2.0.0-p195@nembrotcom/bin:/usr/local/rvm/gems/ruby-2.0.0-p195@nembrot/bin:/usr/local/rvm/rubies/ruby-2.0.0-p195/bin:/usr/local/rvm/bin:$PATH'
-default_environment['GEM_HOME']     = '/usr/local/rvm/gems/ruby-2.0.0-p195@nembrotcom'
-default_environment['GEM_PATH']     = '/usr/local/rvm/gems/ruby-2.0.0-p195@nembrotcom:/usr/local/rvm/gems/ruby-2.0.0-p195@nembrotcom'
-default_environment['RUBY_VERSION'] = 'ruby-2.0.0-p195'
+default_environment['PATH']         = '/usr/local/rvm/gems/ruby-2.1.5-p273@nembrot/bin:/usr/local/rvm/gems/ruby-2.1.5-p273@nembrot/bin:/usr/local/rvm/rubies/ruby-2.1.5-p273/bin:/usr/local/rvm/bin:$PATH'
+default_environment['GEM_HOME']     = '/usr/local/rvm/gems/ruby-2.1.5-p273@nembrot'
+default_environment['GEM_PATH']     = '/usr/local/rvm/gems/ruby-2.1.5-p273@nembrot:/usr/local/rvm/gems/ruby-2.1.5-p273@nembrot'
+default_environment['RUBY_VERSION'] = 'ruby-2.1.5-p273'

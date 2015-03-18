a40a36994dfe877092d9546438403dac4e6c0277
joegatt <joe@joegatt.net>
2015-03-18 23:51:42 +0100

    Remove Paylogger

--- config/initializers/logs.rb
+++ config/initializers/logs.rb
@@ -1,6 +1,5 @@
 pay_logfile = File.open("#{ Rails.root }/log/pay.log", 'a')
 pay_logfile.sync = true
-PAY_LOG = PayLogger.new(pay_logfile)
 
 sync_logfile = File.open("#{ Rails.root }/log/sync.log", 'a')
 sync_logfile.sync = true

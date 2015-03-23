b2570126d4e3fb534a0cb45612a3c787d8d959e6
joegatt <joe@joegatt.net>
2015-03-23 22:56:10 +0100

    Update database

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141222151916) do
+ActiveRecord::Schema.define(version: 20150323212826) do
 
   create_table "authorizations", force: true do |t|
     t.string   "provider"
@@ -204,13 +204,6 @@ ActiveRecord::Schema.define(version: 20141222151916) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "tweet_id",            limit: 8
-  end
-
-  create_table "related_notes", force: true do |t|
-    t.integer  "note_id"
-    t.integer  "related_note_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
   end
 
   create_table "resources", force: true do |t|

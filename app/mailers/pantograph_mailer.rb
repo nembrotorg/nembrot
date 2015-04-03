ed87fff6b31a4533038aa7c403efcef4c7af8707
joegatt <joe@joegatt.net>
2015-04-03 15:31:55 +0200

    Fix from email setting

--- app/mailers/pantograph_mailer.rb
+++ app/mailers/pantograph_mailer.rb
@@ -1,5 +1,5 @@
 class PantographMailer < ActionMailer::Base
-  default from: Constant.admin_email
+  default from: Setting['advanced.admin_email']
 
   def tweet_failed(message)
     @message = message

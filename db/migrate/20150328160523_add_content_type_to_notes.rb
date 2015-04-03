ed48001a848d0b04e295f60249020f71ed6cab6b
joegatt <joe@joegatt.net>
2015-04-03 15:15:13 +0200

    Simplify rake task

--- db/migrate/20150328160523_add_content_type_to_notes.rb
+++ db/migrate/20150328160523_add_content_type_to_notes.rb
@@ -2,10 +2,10 @@ class AddContentTypeToNotes < ActiveRecord::Migration
   def change
     add_column :notes, :content_type, :integer, default: 0, null: false
 
-    Note.all.each do |note|
-      if note.is_citation?
-        note.content_type = 1
-        note.save!
+    Note.all.each do |n|
+      if n.is_citation?
+        n.content_type = 1
+        n.save!
       end
     end
   end

a88eb6d522db2f5f82f65eb773187f9e2d43d127
Joe Gatt <joe@joegatt.net>
2015-06-15 14:31:32 +0200

    Add url helpers to specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -36,6 +36,8 @@ RSpec.configure do |config|
 
   config.include FactoryGirl::Syntax::Methods
 
+  config.include Rails.application.routes.url_helpers
+
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.

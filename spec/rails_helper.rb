ecbdfc5ebf79f024e2741907cfa102ba351cef6a
joegatt <joe@joegatt.net>
2015-08-11 18:59:54 +0200

    Remove duplication

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -9,12 +9,9 @@ require 'rspec/rails'
 
 require 'capybara/rails'
 require 'capybara/rspec'
-require 'codeclimate-test-reporter'
 require 'paper_trail/frameworks/rspec'
 require 'rubygems'
 require 'simplecov'
-
-CodeClimate::TestReporter.start
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are

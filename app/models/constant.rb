7f3c784ce1043ed7d8cc78f5c43732ff1f8032d6
joegatt <joe@joegatt.net>
2015-08-13 22:41:25 +0200

    Add load! to avoid collision with Capistrano

--- app/models/constant.rb
+++ app/models/constant.rb
@@ -5,4 +5,6 @@ class Constant < Settingslogic
   Dir.glob("#{ Rails.root }/config/*.constants.yml") do |extra_settings_file|
     instance.deep_merge!(Constant.new(extra_settings_file))
   end
+
+  load!
 end

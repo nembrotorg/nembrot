cc8d9b4297afb8f5d089e236ce2cded874fc29f4
joegatt <joe@joegatt.net>
2015-08-15 18:50:03 +0200

    Ensure boolean

--- config/initializers/slim.rb
+++ config/initializers/slim.rb
@@ -1,5 +1,5 @@
 # Slim
 Slim::Engine.set_options format: ENV['html_format'].to_sym,
-                         pretty: ENV['html_pretty'],
+                         pretty: ENV['html_pretty'] == 'pretty',
                          sort_attrs: ENV['html_sort_attrs'],
                          tabsize: ENV['html_tabsize'].to_i

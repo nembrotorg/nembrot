4af0428fba02eec5c2a5cce956857daa8dfb6d9f
joegatt <joe@joegatt.net>
2015-08-26 10:16:39 +0200

    Debug whenever

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,13 +1,9 @@
 set :output, "#{ path }/log/daemons.log"
 
-# FIXME: We should be able to do this with stage
-if path == '/home/deployer/apps/joegattnet_v3'
+every 1.hour do
+  rake 'joegattnet:one_hour'
+end
 
-  every 60.minutes do
-    rake '-s joegattnet:one_hour'
-  end
-
-  every 1.day, at: '5:00 am' do
-    rake '-s sitemap:refresh'
-  end
+every 1.day, at: '5:00 am' do
+  rake '-s sitemap:refresh'
 end
